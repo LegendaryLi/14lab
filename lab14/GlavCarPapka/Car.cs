6c127754c5f51cc42b1b8d30d24cd9a5e75d6238
LegendaryLi <cofgtweart@gmail.com>
2022-02-08 12:24:34 +0000

    added 14LabLocalProject

---
+++ lab14/GlavCarPapka/Car.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab14
+{
+    public class Car:INotifyPropertyChanged
+    {
+        private string _model;
+        private int _maxSpeed;
+        private double _discount;
+        private decimal _price;
+        public static string[] Categories { get; } = new string[] { "Легковой", "Грузовой", "Мотоцикл" };
+        private string _category;
+        public string Model
+        {
+            get { return _model; }
+            set
+            {
+                _model = value;
+                OnPropertyChanged("Model");
+            }
+        }
+        public string Category
+        {
+            get { return _category; }
+            set
+            {
+                _category = value;
+                OnPropertyChanged("Category");
+            }
+        }
+        public double Discount { get { return _discount; }
+        set { _discount = value; 
+                OnPropertyChanged("Discount");
+                OnPropertyChanged("PriceWithDiscount");
+            }
+        }
+        public int MaxSpeed { 
+            get { return _maxSpeed; }
+        set { 
+                _maxSpeed = value; 
+                OnPropertyChanged("MaxSpeed"); 
+            }
+        }
+        public decimal Price
+        {
+            get { return _price; }
+            set { _price = value;
+                OnPropertyChanged("Price");
+                OnPropertyChanged("PriceWithDiscount");
+            }
+        }
+        public decimal PriceWithDiscount
+        {
+            get { return Price * (decimal)(1 - Discount);  }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ lab14/GlavCarPapka/CarViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+
+namespace lab14
+{
+    public class CarViewModel:INotifyPropertyChanged
+    {
+        private Car _selectedCar=new Car();
+        public ObservableCollection<Car> Cars { get; set; }
+        public CarViewModel()
+        {
+            using (FileStream fs = new FileStream("cars.json", FileMode.Open))
+            {
+                try
+                {
+                    Cars = JsonSerializer.Deserialize<ObservableCollection<Car>>(fs);
+                } catch { Cars=new ObservableCollection<Car> { }; }
+            }
+        }
+        public Car SelectedCar
+        {
+            get { return _selectedCar; }
+            set { _selectedCar = value;
+                OnPropertyChanged("SelectedCar");
+            }
+        }
+        public void AddCar()
+        {
+            Cars.Insert(0, _selectedCar);
+        }
+        public void DeleteCar()
+        {
+            if (_selectedCar != null)
+            {
+                Cars.Remove(SelectedCar);
+                SelectedCar = new Car();
+            }
+        }
+        public void SetDiscountForCategory()
+        {
+            Cars.Where(x => x.Category == _selectedCar.Category).ToList().ForEach(x => x.Discount = _selectedCar.Discount);
+        }
+        public void RefreshSource()
+        {
+            using (FileStream fs = new FileStream("cars.json", FileMode.Create))
+            {
+                JsonSerializer.Serialize<ObservableCollection<Car>>(fs, Cars);
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
